1ca2c1594e7e4a58e2fd74f53a6d86f2b2cdd9d9
ikhsanalfajar <116376200+ikhsanalfajar@users.noreply.github.com>
2024-01-01 22:14:07 +0700

    Add files via upload

--- streamlit_heart_disease.py
+++ streamlit_heart_disease.py
@@ -1,23 +1,24 @@
 import pickle
 import streamlit as st
 
+
 model = pickle.load(open('heart_disease.sav', 'rb'))
 
 st.title('Prediksi Penyakit Jantung')
 
-age = st.number_input('Input usia')
-sex = st.number_input('Input jenis kelamin')
-cp = st.number_input('Input jenis nyeri dada')
-trestbps	 = st.number_input('Input tekanan darah')
-chol = st.number_input('Input kadar kolesterol')
-fbs = st.number_input('Input gula darah')
-restecg	 = st.number_input('Input hasil elektrokardiogram')
-thalach = st.number_input('Input Denyut jantung maksimum yang dicapai')
-exang = st.number_input('Input Angina akibat olahraga')
-oldpeak = st.number_input('Input oldpeak')
-slope = st.number_input('Input Kemiringan')
-ca = st.number_input('Input Jumlah pembuluh darah utama')
-thal = st.number_input('Input tipe thalassemia')
+age = st.text_input('Input usia')
+sex = st.text_input('Input jenis kelamin')
+cp = st.text_input('Input jenis nyeri dada')
+trestbps = st.text_input('Input tekanan darah')
+chol = st.text_input('Input kadar kolesterol')
+fbs = st.text_input('Input gula darah')
+restecg	 = st.text_input('Input hasil elektrokardiogram')
+thalach = st.text_input('Input Denyut jantung maksimum yang dicapai')
+exang = st.text_input('Input Angina akibat olahraga')
+oldpeak = st.text_input('Input oldpeak')
+slope = st.text_input('Input Kemiringan')
+ca = st.text_input('Input Jumlah pembuluh darah utama')
+thal = st.text_input('Input tipe thalassemia')
 
 
 predict = ''
