88de434d9ec693b163476b9eb01c196cef587e07
ikhsanalfajar <116376200+ikhsanalfajar@users.noreply.github.com>
2024-01-02 17:10:31 +0700

    Add files via upload

--- streamlit_heart_disease.py
+++ streamlit_heart_disease.py
@@ -1,5 +1,6 @@
 import pickle
 import streamlit as st
+from sklearn.neighbors import KNeighborsClassifier
 
 
 model = pickle.load(open('heart_disease.sav', 'rb'))
@@ -9,31 +10,31 @@ st.title('Prediksi Penyakit Jantung')
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    age = st.text_input('Input usia')
+    age = st.number_input('Input usia')
 with col1:
-    sex = st.text_input('Input jenis kelamin')
+    sex = st.number_input('Input jenis kelamin')
 with col1:
-    cp = st.text_input('Input jenis nyeri dada')
+    cp = st.number_input('Input jenis nyeri dada')
 with col1:
-    trestbps = st.text_input('Input tekanan darah')
+    trestbps = st.number_input('Input tekanan darah')
 with col1:
-    chol = st.text_input('Input kadar kolesterol')
+    chol = st.number_input('Input kadar kolesterol')
 with col2:
-    fbs = st.text_input('Input gula darah')
+    fbs = st.number_input('Input gula darah')
 with col2:
-    restecg	 = st.text_input('Input hasil elektrokardiogram')
+    restecg	 = st.number_input('Input hasil elektrokardiogram')
 with col2:
-    thalach = st.text_input('Input Denyut jantung maksimum yang dicapai')
+    thalach = st.number_input('Input Denyut jantung maksimum yang dicapai')
 with col2:
-    exang = st.text_input('Input Angina akibat olahraga')
+    exang = st.number_input('Input Angina akibat olahraga')
 with col2:
-    oldpeak = st.text_input('Input oldpeak')
+    oldpeak = st.number_input('Input oldpeak')
 with col3:
-    slope = st.text_input('Input Kemiringan')
+    slope = st.number_input('Input Kemiringan')
 with col3:
-    ca = st.text_input('Input Jumlah pembuluh darah utama')
+    ca = st.number_input('Input Jumlah pembuluh darah utama')
 with col3:
-    thal = st.text_input('Input tipe thalassemia')
+    thal = st.number_input('Input tipe thalassemia')
 
 
 predict = ''
@@ -42,4 +43,9 @@ if st.button('Prediksi Mempunyai penyakit jantung'):
     predict = model.predict(
         [[age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal]]
     )
+
+    if (predict == 1):
+            st.warning("Orang tersebut aman dari penyakit jantung")
+    else:
+            st.success("Orang tersebut rentan terkena penyakit jantung")
     st.write('Prediksi : ', predict)
